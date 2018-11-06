58cd213c21b9435f9eb966cd714bae6326673a64
Bogdan.Skripnik <skripnik.b@gmail.com>
2018-11-06 17:41:40 +0200

    Task #16 - Done

--- SQL.sql
+++ SQL.sql
@@ -2,6 +2,9 @@
 ?
 Задание № 14
 
-SELECT DISTINCT a.model AS model, b.model AS model, b.speed, b.ram
+SELECT DISTINCT a.model, b.model, b.speed, b.ram
 FROM pc a, pc b
-WHERE a.speed = b.speed AND a.ram = b.ram
+WHERE a.speed = b.speed AND a.ram = b.ram AND a.model > b.model
+
+Задание № 15
+
