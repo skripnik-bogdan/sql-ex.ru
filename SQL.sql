13411a2dff46b6842d9e175a96dd1dc83b908eba
Bogdan.Skripnik <skripnik.b@gmail.com>
2018-11-06 17:54:46 +0200

    no message

--- SQL.sql
+++ SQL.sql
@@ -1,10 +1,10 @@
-Задание № 13
-?
 Задание № 14
+?
+Задание № 16
 
 SELECT DISTINCT a.model, b.model, b.speed, b.ram
 FROM pc a, pc b
 WHERE a.speed = b.speed AND a.ram = b.ram AND a.model > b.model
 
-Задание № 15
+Задание № 17
 
